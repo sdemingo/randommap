aa0a9dc90a5816e1ad87ab93e534b635949b2448
Sergio de Mingo <sdemingo@gmail.com>
2021-01-23 19:38:14 +0100

    mejorando el autotiling

--- map.js
+++ map.js
@@ -9,8 +9,8 @@ let FOREST = 2
 let MOUNTAIN = 3
 let SNOW = 4
 
-let MAX_COLS = 32
-let MAX_ROWS = 32
+let MAX_COLS = 16
+let MAX_ROWS = 16
 
 
 function Tile(type) {
@@ -37,7 +37,7 @@ function getColorByType(tile) {
 
 
 function getColorByHeight(tile) {
-    return "rgba(0,0,0,"+tile.height+")"
+    return "rgba(0,0,0," + tile.height + ")"
 }
 
 
@@ -51,12 +51,53 @@ Tile.prototype.render = function (ctxt, x, y, colorFunc) {
 }
 
 
-Tile.prototype.draw = function (map, col, row) {
+Tile.prototype.draw = function (map, col, row,auto) {
 
-    map.ctxt.drawImage(map.sprites[GRASS],  col * TILE_SZ, row * TILE_SZ)
-    if (this.type != GRASS) {
-        map.ctxt.drawImage(map.sprites[this.type],  col * TILE_SZ, row * TILE_SZ)
+    let mask=15
+    if (auto){
+        mask = getGrassSprite(map, col, row)
     }
+
+    map.ctxt.drawImage(map.sprites[GRASS],
+        0, mask * TILE_SZ,
+        TILE_SZ, TILE_SZ,
+        col * TILE_SZ, row * TILE_SZ,
+        TILE_SZ, TILE_SZ)
+
+
+    if (this.type != GRASS) {
+        map.ctxt.drawImage(map.sprites[this.type], col * TILE_SZ, row * TILE_SZ)
+    }
+}
+
+
+// Autotiling function: Tengo que afinarlo en los bordes
+function getGrassSprite(map, col, row) {
+    if ((col==0) || (col==map.cols-1) || (row==0) || (row==map.rows-1)){
+        return 15 // es borde
+    }
+
+    let a, b, c, d
+    if (row - 1 >= 0) {
+        a = map.grid[col][row - 1]
+    }
+    if (col + 1 < map.cols) {
+        b = map.grid[col + 1][row]
+    }
+    if (row + 1 < map.rows) {
+        c = map.grid[col][row + 1]
+    }
+    if (col - 1 >= 0) {
+        d = map.grid[col - 1][row]
+    }
+
+    let mask = 0
+    if ((a) && a.type == GRASS) { mask += 1 }
+    if ((b) && b.type == GRASS) { mask += 2 }
+    if ((c) && c.type == GRASS) { mask += 4 }
+    if ((d) && d.type == GRASS) { mask += 8 }
+    
+    return mask
 }
 
 
@@ -85,32 +126,32 @@ function Map(titleText) {
     for (var x = 0; x < this.cols; x++) {
         this.grid[x] = new Array()
         for (var y = 0; y < this.rows; y++) {
-            if (Math.floor(Math.random() * 101 < FOREST_AMOUNT)) {
-                this.grid[x][y] = new Tile(FOREST)
-            } else {
-                this.grid[x][y] = new Tile(GRASS)
-            }
+            //if (Math.floor(Math.random() * 101 < FOREST_AMOUNT)) {
+            //    this.grid[x][y] = new Tile(FOREST)
+            //} else {
+            this.grid[x][y] = new Tile(GRASS)
+            //}
 
         }
     }
 
 
     // Cargo sprites
-    let spritesLoaded=0
+    let spritesLoaded = 0
     this.sprites = []
     this.sprites[WATER] = new Image()
-    this.sprites[WATER].onload=function(){spritesLoaded++}
+    this.sprites[WATER].onload = function () { spritesLoaded++ }
     this.sprites[WATER].src = "sprites/water.png"
 
     this.sprites[GRASS] = new Image()
-    this.sprites[GRASS].onload=function(){spritesLoaded++}
-    this.sprites[GRASS].src = "sprites/grass.png"
+    this.sprites[GRASS].onload = function () { spritesLoaded++ }
+    this.sprites[GRASS].src = "sprites/grass-shore.png"
 
     this.sprites[FOREST] = new Image()
-    this.sprites[FOREST].onload=function(){spritesLoaded++}
+    this.sprites[FOREST].onload = function () { spritesLoaded++ }
     this.sprites[FOREST].src = "sprites/tree.png"
 
-    this.spritesReady = function(){
+    this.spritesReady = function () {
         return spritesLoaded == 3
     }
 }
@@ -129,8 +170,8 @@ Map.prototype.isWall = function (r, c) {
 }
 
 
-Map.prototype.render = function (colorFunc) {
-    if (!this.spritesReady()){
+Map.prototype.render = function (autoTiling=true) {
+    if (!this.spritesReady()) {
         console.log("Sprites no cargados aun")
         return
     }
@@ -139,7 +180,7 @@ Map.prototype.render = function (colorFunc) {
     for (var col = 0; col < this.cols; col++) {
         for (var row = 0; row < this.rows; row++) {
             //this.grid[x][y].render(this.ctxt, x, y, colorFunc)
-            this.grid[col][row].draw(this, col, row)
+            this.grid[col][row].draw(this, col, row, autoTiling)
         }
     }
 }
