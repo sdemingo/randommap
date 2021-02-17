1ba8d0b7ea022e95369e78605f03729b97f78635
Sergio de Mingo <sdemingo@gmail.com>
2021-02-17 19:43:07 +0100

    trabajando en dungeons

--- dungeon.js
+++ dungeon.js
@@ -2,37 +2,50 @@
 let MAX_ROOMS = 8
 
 
+const MIN_ROOM_WIDTH = 5
+const MIN_ROOM_HEIGHT = 5
+
 function runDungeonBuilder(map) {
     // Create first space
     let root = new Space(0, 0, map.cols - 1, map.rows - 1)
 
-    // let childs = root.split()
-    // childs[0].insertToMap(map)
-    // childs[1].insertToMap(map)
-
-    // let niet = childs[1].split()
-    // niet[0].insertToMap(map)
-    // niet[1].insertToMap(map)
-
     let c = 0
     divideSpace(map, root, c)
+    drawSpaces(map,root)
 }
+
 
 function divideSpace(map, room, count) {
     if (count > MAX_ROOMS) {
         return
     } else {
         let childs = room.split()
-        childs[0].insertToMap(map)
-        childs[1].insertToMap(map)
-        count+=2
+        if (!childs){
+            return
+        }
         if (childs[0].validSpace()){
+            room.children.push(childs[0])
+            count++
             divideSpace(map, childs[0], count)
         }
         if (childs[1].validSpace()){
+            room.children.push(childs[1])
+            count++
             divideSpace(map, childs[1], count)
         }
     }
+}
+
+function drawSpaces(map,room){
+    if (!room){
+        return
+    }
+    if (room.children.length==0){
+        room.insertToMap(map)
+    }
+    drawSpaces(map,room.children[0])
+    drawSpaces(map,room.children[1])
+    
 }
 
 
@@ -46,7 +59,7 @@ function Space(col, row, width, height) {
 }
 
 Space.prototype.validSpace = function () {
-    return (this.width > 10) && (this.height > 10)
+    return (this.width > MIN_ROOM_WIDTH) && (this.height > MIN_ROOM_WIDTH)
 }
 
 /*
@@ -56,17 +69,33 @@ Space.prototype.validSubtree = function(){
 */
 
 Space.prototype.split = function () {
-    let splitType = getRandomInt(0, 2)
+    let splitType
+
+    if ((this.width >= MIN_ROOM_WIDTH) && (this.height >= MIN_ROOM_HEIGHT)) {
+        splitType = getRandomInt(0, 2)
+    } else {
+        if ((this.width < MIN_ROOM_WIDTH) && (this.height < MIN_ROOM_HEIGHT)) {
+            return
+        } else {
+            if (this.width < MIN_ROOM_WIDTH) {
+                splitType = 0
+            }else{
+                splitType = 1
+            }
+        }
+    }
+
+
     //let splitType = 1
     if (splitType) { // partición vertical
-        let splitCol = getRandomInt(4, this.width-4)
+        let splitCol = getRandomInt(2, this.width - 2)
         let s1 = new Space(this.col, this.row, splitCol, this.height)
-        let s2 = new Space(this.col + (splitCol ), this.row, this.width - (splitCol ), this.height)
+        let s2 = new Space(this.col + (splitCol), this.row, this.width - (splitCol), this.height)
         return [s1, s2]
     } else {        // partición horizontal
-        let splitRow = getRandomInt(4, this.height-4)
+        let splitRow = getRandomInt(2, this.height - 2)
         let s1 = new Space(this.col, this.row, this.width, splitRow)
-        let s2 = new Space(this.col, this.row + (splitRow ), this.width, this.height - (splitRow ))
+        let s2 = new Space(this.col, this.row + (splitRow), this.width, this.height - (splitRow))
         return [s1, s2]
     }
 }
@@ -77,10 +106,10 @@ Space.prototype.insertToMap = function (map) {
         for (let r = 0; r <= this.height; r++) {
             let tile = map.getTile(this.row + r, this.col + c)
             //console.log(tile)
-            if (tile){
-                if ((c == 0) || (r == 0) || (c == this.width)|| (r == this.height)){
+            if (tile) {
+                if ((c == 0) || (r == 0) || (c == this.width) || (r == this.height)) {
                     tile.type = STONE_WALL
-                }else{
+                } else {
                     tile.type = STONE_FLOOR
                 }
             }
